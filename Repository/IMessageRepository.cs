482268a328ed109734c7b8f39caebc4f14300c18
jorgeedvaldo <edvaldojorge19962gmail.com>
2020-04-26 15:17:16 +0100

    Actualizando o reposit]orio

--- Repository/IMessageRepository.cs
+++ Repository/IMessageRepository.cs
@@ -1,7 +1,17 @@
+using EmpregosYoyotaAPI.Models;
+
 namespace EmpregosYoyotaAPI.Repository
 {
     public interface IMessageRepository
     {
-         
+         void Add(Message message);
+
+         IEnumerable<Message> GetAll();
+
+         Message Find(long id);
+
+         void Remove(long id);
+
+         void Update(Message message);
     }
 }

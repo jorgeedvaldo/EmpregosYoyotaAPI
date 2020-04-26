e04d50396d44beefb26e3f51d76e16692d016c07
jorgeedvaldo <edvaldojorge19962gmail.com>
2020-04-26 14:56:17 +0100

    Conexão com MSQL

--- Startup.cs
+++ Startup.cs
@@ -27,10 +27,7 @@ namespace EmpregosYoyotaAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = Configuration["ConexaoMySql:MySqlConnectionString"];
-            services.AddDbContext<DataContext>(options =>
-                options.UseMySql(connection)
-            );
+            services.AddDbContext<DataContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
         }
